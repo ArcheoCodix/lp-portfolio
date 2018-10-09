72036cac02de48e8145dccd176e76e5912bb9725
Nils Toularastel <toularan@iut1i-702-a18.local>
2018-10-09 17:38:12 +0200

    NightMod + transition

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-bite
+$(function(){
+    $("#buttonJourNuit").mousedown(function () { 
+        $("#buttonJourNuit").addClass("ClickOn");
+    });
+
+    $("#buttonJourNuit").mouseup(function () { 
+        $("#buttonJourNuit").removeClass("ClickOn");
+    });
+
+    $("#buttonJourNuit").click(function () { 
+        if ($("body").hasClass("Nuit"))
+        {
+            $("#buttonJourNuit").text("Mode Nuit");
+            $("body").removeClass("Nuit");
+            $("header").removeClass("Nuit");
+        }
+        else
+        {
+            $("#buttonJourNuit").text("Mode Jour");
+            $("body").addClass("Nuit");
+            $("header").addClass("Nuit");
+        }
+    });
+
+});
+
