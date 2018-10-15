c31b26befd614c7dfc48e1d09d49b6a25ab6c5fa
Nils Toularastel <toularan@wificampus-116005.grenet.fr>
2018-10-15 12:17:18 +0200

    le truc

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
-$(function(){
+var i = 0;
+var fin = false;
+var timeout;
+
+$(document).ready(function(){
+
     $("#buttonJourNuit").mousedown(function () { 
         $("#buttonJourNuit").addClass("ClickOn");
     });
@@ -7,20 +12,68 @@ $(function(){
         $("#buttonJourNuit").removeClass("ClickOn");
     });
 
-    $("#buttonJourNuit").click(function () { 
-        if ($("body").hasClass("Nuit"))
-        {
-            $("#buttonJourNuit").text("Mode Nuit");
-            $("body").removeClass("Nuit");
-            $("header").removeClass("Nuit");
-        }
-        else
-        {
-            $("#buttonJourNuit").text("Mode Jour");
-            $("body").addClass("Nuit");
-            $("header").addClass("Nuit");
-        }
-    });
+    $("#buttonJourNuit").click(clickLightDark);
 
+    var today = new Date();
+    today.setHours(21);
+
+    if(today.getHours() >= 20)
+        clickLightDark();
 });
 
+function clickLightDark()
+{ 
+    var tabCouleurNuit = ["#eedfb5","#ecd490","#e9c667","#a78e4c","#5a5139"];
+    
+    if ($("body").hasClass("Nuit"))
+    {
+        i = tabCouleurNuit.length-1;
+        fin = false;
+        timeout = setInterval(changeColorTimer, 80, tabCouleurNuit, false);
+    }
+    else
+    {
+        i = 0;
+        fin = false;
+        timeout = setInterval(changeColorTimer, 80, tabCouleurNuit, true);
+    }
+}
+
+function changeColorTimer(tabColor, sensPlus)
+    {
+        if (i < tabColor.length && i >= 0 )
+            $("body").css("background-color",tabColor[i]);
+        else
+            {
+                $("body").css("background-color","");
+                
+                if (sensPlus == false)
+                {
+                    $("#buttonJourNuit").text("Mode Nuit");
+                    $("title").text("Présentation (Bonjour)");
+                    $("body").removeClass("Nuit");
+                    $("header").removeClass("Nuit");
+                    $("#buttonJourNuit").removeClass("Nuit");
+                }
+                else
+                {
+                    $("#buttonJourNuit").text("Mode Jour");
+                    $("body").addClass("Nuit");
+                    $("header").addClass("Nuit");
+                    $("#buttonJourNuit").addClass("Nuit");
+                    $("title").text("Présentation (Bonsoir)");
+                }
+                
+                setTimeout(leClear, 1);
+            }
+        
+        if (sensPlus == true)
+            i++;
+        else
+            i--;
+    }
+
+    function leClear()
+    {
+        clearInterval(timeout);
+    }
